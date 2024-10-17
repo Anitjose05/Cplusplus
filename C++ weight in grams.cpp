08e4c874b978876b522484976cf52fcaf9840df9
Anit Jose <166154249+Anitjose05@users.noreply.github.com>
2024-10-17 12:29:24 +0530

    Add files via upload

---
+++ C++ weight in grams.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  float w;
+  float kg;
+  cout<<"Enter the weight in grams:";
+  cin>>w;
+  kg=w/1000;
+  cout<<"kilogram=" <<kg <<"kg:";
+  return 0;
+}
+  
+
+  
